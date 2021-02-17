9bde95e0157594279eed02f4a8ab53e6be03ea7b
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2021-02-17 16:10:42 +0900

    ReactRouterの導入、Jsxファイルの生成

---
+++ frontend/src/components/Builds.jsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+export const Builds = () => {
+  return (
+    <Fragment>
+      レンタルスペース一覧
+    </Fragment>
+  )
+}
---
+++ frontend/src/components/Reserves.jsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+export const Reserves = () => {
+  return (
+    <Fragment>
+      予約画面
+    </Fragment>
+  )
+}
