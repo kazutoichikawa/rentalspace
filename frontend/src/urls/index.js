47067df1709dd4cffa6682597a9ca43ef0a7b12f
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2021-02-17 16:32:20 +0900

    apiを叩くためのURLを定義

---
+++ frontend/src/urls/index.js
@@ -0,0 +1,8 @@
+const DEFAULT_API_LOCALHOST = 'http://localhost:3000/api/v1'
+
+export const buildsIndex = `${DEFAULT_API_LOCALHOST}/builds`
+export const roomIndex = (buildId) =>
+  `${DEFAULT_API_LOCALHOST}/builds/${buildId}/rooms`
+export const PreReserve = `${DEFAULT_API_LOCALHOST}/pre_reserves`;
+export const PreReserveReplace = `${DEFAULT_API_LOCALHOST}/pre_reserves/replace`;
+export const reserves = `${DEFAULT_API_LOCALHOST}/reserves`;
