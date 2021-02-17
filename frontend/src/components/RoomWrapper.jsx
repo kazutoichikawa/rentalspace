308fd8da94dfacde40208f9c6cd6f12a6c246f5d
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2021-02-18 04:49:38 +0900

    スタイルの適用

---
+++ frontend/src/components/RoomWrapper.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { SubText } from '../styles/StyledText';
+import { COLORS } from '../style_constants';
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 450px;
+  height: 180px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${COLORS.BORDER};
+  border-image: initial;
+  cursor: pointer;
+`;
+
+const RoomDetail = styled.div`
+  padding: 24px 16px;
+  width: 250px;
+`;
+
+const DescriptionWrapper = styled.div`
+  height: 75px;
+`
+
+const PriceWrapper = styled.div`
+  margin-top: 16px;
+`
+
+const RoomImageNode = styled.img`
+  width: 250px;
+`;
+
+export const RoomWrapper = ({
+  room,
+  onClickRoomWrapper,
+  imageUrl,
+}) => (
+  <Wrapper onClick={() => onClickRoomWrapper(room)}>
+    <RoomDetail>
+      {room.name}
+      <PriceWrapper>
+         <SubText>最大収容人数：{room.capacity}人</SubText>
+         <SubText>60分/{room.price}円</SubText>
+         <SubText>{room.detail}</SubText>
+      </PriceWrapper>
+    </RoomDetail>
+    <RoomImageNode src={imageUrl} />
+  </Wrapper>
+)
--- frontend/src/components/Rooms.jsx
+++ frontend/src/components/Rooms.jsx
@@ -1,11 +1,50 @@
 import React, { Fragment, useEffect, useReducer } from 'react';
 import { fetchRooms } from '../apis/rooms';
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
 import {
   initialState as roomsInitialState,
   roomsActionTyps,
   roomsReducer,
 } from '../reducers/rooms';
 import { REQUEST_STATE } from '../constants';
+import { RoomWrapper } from './RoomWrapper';
+import Skeleton from '@material-ui/lab/Skeleton';
+
+import { COLORS } from '../style_constants';
+import { LocalMallIcon } from '../Icons';
+
+import MainLogo from '../images/logo.png';
+import RoomImage from '../images/room-image.png';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const RoomsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
 
 export const Rooms = ({
   match
@@ -25,20 +64,40 @@ export const Rooms = ({
   }, [])
   return (
     <Fragment>
-      {
-        roomsState.fetchState === REQUEST_STATE.LOADING ?
-          <Fragment>
-            <p>
-              ロード中...
-            </p>
-          </Fragment>
-        :
-          roomsState.roomsList.map(room =>
-            <div key={room.id}>
-              {room.name}
-            </div>
-          )
-      }
+      <HeaderWrapper>
+        <Link to="/builds">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="/reserves">
+            <ColoredBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <RoomsList>
+        {
+          roomsState.fetchState === REQUEST_STATE.LOADING ?
+            <Fragment>
+              {
+                [...Array(12).keys()].map(i =>
+                  <ItemWrapper key={i}>
+                    <Skeleton key={i} variant="rect" width={450} height={180} />
+                  </ItemWrapper>
+                )
+              }
+            </Fragment>
+          :
+            roomsState.roomsList.map(room =>
+              <ItemWrapper key={room.id}>
+                <RoomWrapper
+                  room={room}
+                  onClickRoomWrapper={(room) => console.log(room)}
+                  imageUrl={RoomImage}
+                />
+              </ItemWrapper>
+            )
+        }
+      </RoomsList>
     </Fragment>
   )
   }
